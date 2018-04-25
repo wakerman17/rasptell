edd1f47478275ed7744e0fb3b7cc77a768658431
FelixsGit <28272254+FelixsGit@users.noreply.github.com>
2018-04-25 10:49:46 +0200

    Add PagesController for pages navigation

---
+++ resources/views/pages/control.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}"
+        <title>{{config('App.name')}}</title>
+        <title>Rasptell</title>
+    </head>
+    <body>
+      <div class="topnav">
+        <a href="/">Home</a>
+      <a href="/register">Register</a>
+        <a href="/control">Control</a>
+    </div>
+      <h1>This is the Control Page!</h1>
+      <h3>Here you can control your devices</h3>
+    </body>
+</html>
