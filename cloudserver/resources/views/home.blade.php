23d8cf8b126a6d69bcfe995d8e7c42bea5b692af
William Åkerman <wakerman@kth.se>
2018-05-08 13:33:20 +0200

    Sec 3

--- cloudserver/resources/views/home.blade.php
+++ cloudserver/resources/views/home.blade.php
@@ -5,19 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Rasptell control center</div>
-
+                <div class="card-header">Rasptell kontrollcenter</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     <div class="form-group row mb-0">
                         <div class="col-md-6 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Light switch') }}
+                            <button type="submit" id="lightOn" class="btn btn-primary">
+                                {{ __('ON') }}
+                            </button>
+                            <button type="submit" id="lightOff" class="btn btn-primary">
+                                {{ __('OFF') }}
                             </button>
                         </div>
                     </div>
