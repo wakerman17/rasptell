1f04d9738e9cedfe28642da9eb676b7f67de4a7f
anga13 <kalle.spik@hotmail.com>
2018-05-15 22:46:00 +0200

    Add error message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,11 +12,10 @@
                             {{ session('status') }}
                         </div>
                     @endif
-					<?php //echo "$flag" 
-					?>
+					@php ($i=0)
 					@if ($flag === 1 || $flag === 4)
 						@if (count($user_devices) > 0)
-						@php ($i=0)
+						
 							@foreach($user_devices as $user_device)
 								<input type="hidden" id="ip-address" value="{{$ip}}">
 								<div class="form-group row mb-0">
@@ -34,37 +33,38 @@
 								@php ($i++)
 							@endforeach
 						@else
-							<small>Du kan inte ändra någon lampa eftersom det finns inga enheterna registrerade. Ledsen {{Auth::user()->name}}</small>
+							<small>Du kan inte ändra någon lampa eftersom det finns inga enheterna registrerade. Ledsen {{Auth::user()->name}}.</small>
 						@endif
 					@endif
 					@if  ($flag === 2)
-						<small>Du kan inte ändra enheterna eftersom du inte har någon Raspberry registrerad. Ledsen {{Auth::user()->name}}</small>
+						<small>Du kan inte ändra enheterna eftersom du inte har någon Raspberry registrerad. Ledsen {{Auth::user()->name}}.</small>
 					@endif
 					@if ($flag === 3)
-						<small>Du kan inte ändra någon enheterna eftersom du inte valt en av dina raspberrys. Ledsen {{Auth::user()->name}}</small>
+						<small>Du kan inte ändra någon enheterna eftersom du inte valt en av dina raspberrys. Ledsen {{Auth::user()->name}}.</small>
 					@endif
 					@if ($flag === 5)
-						<small>Du kan inte ändra någon enheterna eftersom det finns inga lampor registrerade. Ledsen {{Auth::user()->name}}</small>
+						<small>Du kan inte ändra någon enheterna eftersom det finns inga lampor registrerade. Ledsen {{Auth::user()->name}}.</small>
 					@endif
 				</div>
 			</div>
-			@if ($flag === 3 || $flag === 4 || $flag === 5)
+			@if ($flag === 3 || $flag === 4)
 				<div class="card">
 					<div class="card-header">Ändra rasp</div>
 					<div class="card-body">
 						<form method="POST" action="{{ route('severalRasps') }}">
 						@csrf
-							@for($i = 0; $i < count($user_rasp_accesses); $i++)
+						@php ($i*=2)
+							@for($j = 0; $j < count($user_rasp_accesses); $j++)
 								<div class="form-group row mb-0">
 									<div class="col-md-6 offset-md-4">
-										<!--<small>{{$user_rasp_accesses[$i]->ip_address}}  </small>-->
 										<input type="hidden" ></input>
-										<button name="ip_address" type="submit" value="{{$user_rasp_accesses[$i]->ip_address}}" id="<?php echo $i; ?>" class="lightOn btn btn-primary">
-												{{ __($user_rasp_accesses[$i]->ip_address) }}
+										<button name="ip_address" type="submit" value="{{$user_rasp_accesses[$j]->ip_address}}" id="{{ $i }}" class="btn btn-primary">
+												{{ __($user_rasp_accesses[$j]->ip_address) }}
 										</button>
 										<br><br>
 									</div>
 								</div>
+								@php ($i++)
 							@endfor
 						</form>
 					</div>
