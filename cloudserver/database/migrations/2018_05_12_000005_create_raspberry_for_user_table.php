3ecfe8dc84baffe28e26b8fc4f05bb49189d9842
William Åkerman <wakerman@kth.se>
2018-05-23 13:32:55 +0200

    user, not users

--- cloudserver/database/migrations/2018_05_12_000005_create_raspberry_for_user_table.php
+++ cloudserver/database/migrations/2018_05_12_000005_create_raspberry_for_user_table.php
@@ -17,7 +17,7 @@ class CreateRaspberryForUserTable extends Migration
             $table->integer('user_id')->unsigned();
 			$table->integer('raspberry_id')->unsigned();
 			$table->primary(['user_id', 'raspberry_id']);
-			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('user_id')->references('id')->on('user');
 			$table->foreign('raspberry_id')->references('id')->on('raspberry');
 			
             $table->timestamps();
