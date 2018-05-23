ab20bcdbb943d7f195b8a1373f9c97e5d92ab95b
William Åkerman <wakerman@kth.se>
2018-05-23 12:12:40 +0200

    Ändringar efter tips från Anders G

--- cloudserver/resources/views/layouts/app.blade.php
+++ cloudserver/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.nameg', 'Rasptell') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -26,7 +26,7 @@
             <div class="container">
               @guest
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.nameg', 'Rasptell') }}
+                    {{ config('app.name') }}
                 </a>
                 @else
                 <a class="navbar-brand" href="{{ url('/home') }}">
